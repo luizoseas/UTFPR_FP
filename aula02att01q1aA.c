8a042fd358f2b5786da356f0e2a05fa15e597db1
Luiz Oséas <luizoseas@alunos.utfpr.edu.br>
2022-03-11 09:02:58 -0300

    Create aula02att01q1aA.c

---
+++ aula02att01q1aA.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    char sexo;
+    printf("Escolha seu SEXO: M ou F\n");
+    scanf("%c",&sexo);
+    printf("\nSeu sexo é: %c",sexo);
+    return 0;
+}
