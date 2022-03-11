52cd0facbba24385c350e0279ad058c8fb5737ff
Luiz Oséas <luizoseas@alunos.utfpr.edu.br>
2022-03-11 09:05:40 -0300

    Create aula02att01q1aB.c

---
+++ aula02att01q1aB.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    char titulolivro[255];
+    printf("Digite o título do livro: \n");
+    scanf("%s",titulolivro);
+    printf("\nTítulo do livro é %s",titulolivro);
+    return 0;
+}
